5882897bb6e188b9bc299875d756b54750c3fec4
zhouyu0314 <181332969@qq.com>
2021-02-10 13:52:23 +0800

    订单 1

---
+++ foodie-dev-api/src/main/java/com/zy/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.zy.controller;
+
+import com.zy.pojo.bo.SubmitOrderBO;
+import com.zy.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "订单相关", tags = {"订单相关的api接口"})
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
+
+
+    @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
+    @PostMapping("/create")
+    public IMOOCJSONResult create(@RequestBody SubmitOrderBO submitOrderBO) {
+        try {
+            //1.创建订单
+            //2.创建订单以后，一处购物车中已结算（已提交）的商品
+            //3.向支付中心发送当前订单，用于保存支付中心的订单数据
+            return IMOOCJSONResult.ok();
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.error(e.getMessage());
+            return IMOOCJSONResult.errorMsg("服务异常！");
+        }
+    }
+
+}
--- foodie-dev-pojo/src/main/java/com/zy/pojo/bo/SubmitOrderBO.java
+++ foodie-dev-pojo/src/main/java/com/zy/pojo/bo/SubmitOrderBO.java
@@ -1,16 +1,24 @@
 package com.zy.pojo.bo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 
 /**
  * 用于创建订单的BO对象
  */
+@ApiModel(value = "订单对象BO", description = "从客户端，由用户传入的数据封装在此entity中")
 public class SubmitOrderBO implements Serializable {
-
+    @ApiModelProperty(value = "用户id", name = "userId", required = true)
     private String userId;
+    @ApiModelProperty(value = "商品规格id", name = "itemSpecIds", required = true)
     private String itemSpecIds;
+    @ApiModelProperty(value = "地址id", name = "addressId", required = true)
     private String addressId;
+    @ApiModelProperty(value = "支付方式", name = "payMethod", required = true)
     private Integer payMethod;
+    @ApiModelProperty(value = "买家留言", name = "leftMsg", required = true)
     private String leftMsg;
 
     public String getUserId() {
