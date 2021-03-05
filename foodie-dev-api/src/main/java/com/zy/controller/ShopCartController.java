0a7d6e21610fe255eef12f058b5b559013884f3d
zy <181332929@qq.com>
2021-03-05 10:53:13 +0800

    test

--- foodie-dev-api/src/main/java/com/zy/controller/ShopCartController.java
+++ foodie-dev-api/src/main/java/com/zy/controller/ShopCartController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/shopcart")
 public class ShopCartController extends BaseController {
+    //asdasdasda
     @Autowired
     RedisOperator redisOperator;
 
