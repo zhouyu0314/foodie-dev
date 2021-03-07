23c108b2d6af4dd2d31dd99f2f12e065ba4e81ee
zy <181332929@qq.com>
2021-03-07 09:42:00 +0800

    单点登录

--- foodie-dev-api/src/main/java/com/zy/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/zy/controller/HelloController.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpSession;
 @RestController
 @CrossOrigin
 public class HelloController {
-
+1
      private static final Logger LOGGER =  LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/hello")
